3f06c5279f72f91284abd7eb42dc0f682e1de313
EDUC.Mart1527 <EDUC.Mart1527@msu.edu>
2017-01-31 14:57:22 -0500

    Fixed required issues

--- liveUpdate.js
+++ liveUpdate.js
@@ -1,41 +1,41 @@
-var fnameInput = document.getElementById('fname')
-var lnameInput = document.getElementById('lname')
+var firstNameInput = document.getElementById('fname')
+var lastNameInput = document.getElementById('lname')
 var bioInput = document.getElementById('bio')
 var emailInput = document.getElementById('email')
 var phoneInput = document.getElementById('phone')
-var previewlib = document.getElementById('preview-lib')
-var rawlib = document.getElementById('raw-lib')
+var previewLib = document.getElementById('preview-lib')
+var rawLib = document.getElementById('raw-lib')
 
 var updateProfile = function () {
-  var fname = fnameInput.value
-  var lname = lnameInput.value
+  var firstName = firstNameInput.value
+  var lastName = lastNameInput.value
   var bio = bioInput.value
   var email = emailInput.value
   var phone = phoneInput.value
 
-  previewlib.innerHTML = 
-  '<h1>Hi, my name is ' + fname + ' ' + lname + '!</h1> ' +
-  '<p>' + bio + '</p> ' +
-  '<p>' +
-    'If you\'re interested in a date, you can email me at ' +
-    '<a href="mailto:' + email + '" target="_blank">' + email + '</a> ' +
-    'or give me a call at ' +
-    '<a href="tel:' + phone + '" target="_blank">' + phone + '</a>.' +
-  '</p>'
+  previewLib.innerHTML = 
+    '<h1>Hi, my name is ' + firstName + ' ' + lastName + '!</h1> ' +
+    '<p>' + bio + '</p> ' +
+    '<p>' +
+      'If you\'re interested in a date, you can email me at ' +
+      '<a href="mailto:' + email + '" target="_blank">' + email + '</a> ' +
+      'or give me a call at ' +
+      '<a href="tel:' + phone + '" target="_blank">' + phone + '</a>.' +
+    '</p>'
 
-  rawlib.textContent = 
-  '<h1>Hi, my name is ' + fname + ' ' + lname + '!</h1> ' +
-  '<p>' + bio + '</p> ' +
-  '<p>' +
-    'If you\'re interested in a date, you can email me at ' +
-    '<a href="mailto:' + email + '" target="_blank">' + email + '</a> ' +
-    'or give me a call at ' +
-    '<a href="tel:' + phone + '" target="_blank">' + phone + '</a>.' +
-  '</p>'
+  rawLib.textContent = 
+    '<h1>Hi, my name is ' + firstName + ' ' + lastName + '!</h1> ' +
+    '<p>' + bio + '</p> ' +
+    '<p>' +
+      'If you\'re interested in a date, you can email me at ' +
+      '<a href="mailto:' + email + '" target="_blank">' + email + '</a> ' +
+      'or give me a call at ' +
+      '<a href="tel:' + phone + '" target="_blank">' + phone + '</a>.' +
+    '</p>'
 }
 
-fnameInput.addEventListener('input', updateProfile)
-lnameInput.addEventListener('input', updateProfile)
+firstNameInput.addEventListener('input', updateProfile)
+lastNameInput.addEventListener('input', updateProfile)
 bioInput.addEventListener('input', updateProfile)
 emailInput.addEventListener('input', updateProfile)
 phoneInput.addEventListener('input', updateProfile)
